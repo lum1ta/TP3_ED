5c5d575e12924a7b71342707105a6bde88f37297
lum1ta <lumamartinsguimaraes@gmail.com>
2025-12-05 16:40:58 -0300

    TAD Address

--- include/Address.h
+++ include/Address.h
@@ -1,21 +1,40 @@
 #define ADDRESS.H
-#if"ndef ADDRESS.H
+#ifndef ADDRESS.H
 
 #include <string>
 
-struct Address{
-    //Código alfanumérico unico para cada endereço
-    std::string idAdd;
-    //Id unico numerico para cada logradouro
-    int idLog;
-    std::string typeLog;
-    std::string Log;
-    int num;
-    std::string neigh;
-    std::string region;
-    int CEP;
-    double Lat;
-    double Long;
-}
+class Address{
+    private:
+        //Código alfanumérico unico para cada endereço
+        std::string idAdd;
+        //Id unico numerico para cada logradouro
+        int idLog;
+        std::string typeLog;
+        std::string Log;
+        int num;
+        std::string neigh;
+        std::string region;
+        int CEP;
+        double Lat;
+        double Long;
+    public:
+        //Constructor
+        Address();
+        ~Addres();
 
-#endif"
+        //Ler os dados de entrada
+        void ReadAdd(string linha);
+
+        //Getters
+        std::string getidLog();
+        id getidLog();
+        std::string gettypeLog();
+        std::string getLog();
+        int getnum();
+        std::string getneigh();
+        std::string getregion();
+        int getCEP();
+        double getLat();
+        double getLong();
+}
+#endif
--- src/Address.cpp
+++ src/Address.cpp
@@ -2,23 +2,3 @@
 #include <iostream>
 #include <fstream>
 #include <string>   // se for usar string
-
-// Leitura do arquivo de entrada
-void Ler() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-
-    std::ifstream file("input.txt");
-
-    if (!file.is_open()) {
-        std::cerr << "Erro ao abrir input.txt\n";
-        return;
-    }
-
-    std::string linha;
-    while (getline(file, linha)) {
-        std::cout << linha << "\n";
-    }
-
-//Cálculo coordenadas médias
-}
